82d8cdeb19704bbb4c69f618d447dcdbac64b23a
mateuhade <tartarohades552@gmail.com>
2026-03-27 15:58:17 -0300

    gitignore update e comeco de linguagem C

---
+++ CursoLogica/Linguagens/C/teste1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {   // todos os códigos em C devem ter essa linha, a "main" diz o que o sistema deve ler quando o programa for executado; o int diz que o programa inteiro ao fim de sua execução retornará
+               // um único número interger, esse número indica se ocorreu algum erro no sistema e qual tipo de erro ocorreu, caso não tenha ocorrido nenhum erro, a última linha do programa é acionada
+
+    printf("Ola mundo!");
+
+
+    return 0;  // essa linha faz o interger da main retornar 0, indicando que nenhum erro ocorreu e que o sistema correu devidamente
+}
